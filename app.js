6192cec9be8c09b4968a4f088248f6671959e10b
Abdurahmon <rahmlow.1217@mail.ru>
2025-01-15 18:51:03 +0500

    1filesdaads

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const musicContainer = document.querySelector("#music-container");
 const playBtn = document.querySelector("#play");
 const prevBtn = document.querySelector("#prev");
-const nextBtn = document.querySelector("#next");
+const nextBtn = document.querySelector("next");
 
 const audio = document.querySelector("#music");
 const progress = document.querySelector("#progress");
@@ -9,72 +9,5 @@ const progressContainer = document.querySelector("#progress-container");
 const title = document.querySelector("#title");
 const cover = document.querySelector("#cover");
 
-const songs = ["Arabic Music", "Eminem Rihanna", "Snoop Dogg"];
-let songIndex = 0;
-
-loadSong(songs[songIndex]);
-function loadSong(song) {
-    title.innerText = song;
-    audio.src = `music/${song}.mp3`;
-    cover.src = `images/${song}.png`;
-}
-
-playBtn.addEventListener("click", () => {
-    const isPlaying = musicContainer.classList.contains("play");
-    if (isPlaying) {
-        pauseSong();
-    } else {
-        playSong();
-    }
-});
-
-function playSong () {
-    musicContainer.classList.add("play");
-    playBtn.querySelector("i.fas").classList.remove("fa-play");
-    playBtn.querySelector("i.fas").classList.add("fa-pause");
-    audio.play();
-}
-function pauseSong() {
-    musicContainer.classList.remove("play");
-    playBtn.querySelector("i.fas").classList.add("fa-play");
-    playBtn.querySelector("i.fas").classList.remove("fa-pause");
-    audio.pause();
-}
-
-prevBtn.addEventListener("click", prevSong);
-nextBtn.addEventListener("click", nextSong);
-
-function prevSong() {
-    songIndex--; 
-    if (songIndex < 0) {
-        songIndex = songs.length - 1
-    }
-    loadSong(songs[songIndex]);
-    playSong();
-
-}
-function nextSong() {
-    songIndex++; 
-    if (songIndex > songs.length - 1) {
-        songIndex = 0;
-    }
-    loadSong(songs[songIndex]);
-    playSong();
-}
-
-audio.addEventListener("timeupdate", updateProgress);
-function updateProgress(e) {
-    const { duration, currentTime } = e.srcElement;
-
-    const progressPercent = (currentTime / duration) * 100;
-    progress.style.width = `${progressPercent}%`;
-}
-
-progressContainer.addEventListener("click", setProgress);
-function setProgress (e) {
-    const width = this.clientWidth;
-    const clickX = e.offsetX;
-    const duration = audio.duration; 
-    audio.currentTime = (clickX / width) * duration;
-}
-
+const songs = ["Arab Music", "Eminem Rihanna", "Snoop Dogg",];
+let
