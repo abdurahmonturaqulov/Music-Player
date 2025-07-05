4e466424147e447dee2467703a04441dc6da9f74
Abdurahmon <rahmlow.1217@mail.ru>
2025-07-05 14:26:39 +0500

    2fileadd

--- app.js
+++ app.js
@@ -26,3 +26,19 @@ function loadSong (song) {
   audio.src = `music/${song}.mp3`
   cover.src = `images/${song}.png`
 }
+
+playBtn.addEventListener('click', () => {
+  const isPlaying = musicContainer.classList.contains('play')
+  if (isPlaying) {
+    pauseSong()
+  } else {
+    playSong()
+  }
+})
+
+function playSong () {
+  musicContainer.classList.add('play')
+  playBtn.querySelector('i.fas').classList.remove('fa-play')
+  playBtn.querySelector('i.fas').classList.add('fa-pause')
+  audio.play()
+}
